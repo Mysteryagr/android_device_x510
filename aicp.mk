22d0988a7f0b1fe9242eca8710731d68b3163b55
mysteryagr <ahmedrahmy842@gmail.com>
2017-06-22 01:31:09 +0530

    Brining up AICP branch

--- aicp.mk
+++ aicp.mk
@@ -3,15 +3,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := x510
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common aicp stuff.
+$(call inherit-product, vendor/aicp/configs/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/infinix/x510/device_x510.mk)
+$(call inherit-product, device/infinix/x510/aicp_x510.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x510
-PRODUCT_NAME := cm_x510
+PRODUCT_NAME := aicp_x510
 PRODUCT_BRAND := INFINIX
 PRODUCT_MODEL := X510
 PRODUCT_MANUFACTURER := INFINIX
